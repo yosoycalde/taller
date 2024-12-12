e5c8782e3e8ab5889dd4b5a7ad2c566128b8c508
yosoycalde <juancalderon2005.18@gmail.com>
2024-12-11 20:29:31 -0500

    1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express =require("express");
+const express = require("express");
 const mysql = require("mysql");
 
 const app = express();
@@ -9,23 +9,43 @@ app.use(express.json());
 
 //Configuracion para la conexio mysql
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "practica"    
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "practica",
+});
 
 //conexion ala base de datos
 db.connect((err) => {
-    if (err) {
-        console.error("ERROR AL CONECTAR A LA BASE DDE DATOS",err);
-    } else {
-        console.log("Conectado a la base de datos");
+  if (err) {
+    console.error("ERROR AL CONECTAR A LA BASE DDE DATOS", err);
+  } else {
+    console.log("Conectado a la base de datos");
+  }
+});
+//crear una nueva materia
+app.post("/materias", (req, res) => {
+  const materia = req.body;
+  const sql =
+    "INSERT INTO materias (NombreMateria, Descripcion, Nota) VALUES(?, ?, ?)";
+  db.query(
+    sql,
+    [materia.NombreMateria, materia.Descripcion, materia.Nota],
+    (err, result) => {
+      if (err) {
+        console.error("Error al crear una nueva materia:", err);
+        res.status(500).json({ error: "Error al crear una nueva materia" });
+      } else {
+        console.log("Materia crada con exito");
+        res
+          .status(201)
+          .json({ massage: "Materia creada", id: result.insertId });
+      }
     }
-})
-
+  );
+});
 
 //iniciar el servidor
-app.listen(port,() => {
-    console.log(`servidor corriendo en http//localhost:${port}`)
+app.listen(port, () => {
+  console.log(`servidor corriendo en http//localhost:${port}`);
 });
